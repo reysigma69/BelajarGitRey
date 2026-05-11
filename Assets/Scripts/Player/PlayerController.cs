f801bd35f0836b8c171770689ed31db3f0491bd3
reysigma69 <reynandasigma69@gmail.com>
2026-05-11 22:02:50 +0700

    fix: implementasi ScriptableObject untuk PlayerData dan sistem GameState

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,26 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    public PlayerData data;
+    private float currentHP;
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        if (data != null)
+        {
+            currentHP = data.maxHP;
+        }
+        else
+        {
+            Debug.LogError("PlayerData belum dipasang di PlayerController!");
+        }
     }
     
     
     void Update()
     {
+        if (GameManager.Instance != null && GameManager.Instance.currentState == GameState.Paused) return;
         if (playerInput == null) return;
         
         moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
         float h = moveInput.x;
         float v = moveInput.y;
 
-        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+        transform.Translate(new Vector3(h, v, 0) * data.moveSpeed * Time.deltaTime);
     }
 
     void OnCollisionStay2D(Collision2D collision)
