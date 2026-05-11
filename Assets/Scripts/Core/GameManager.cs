0017867147819f7b85a634754a35735de0e2b632
reysigma69 <reynandasigma69@gmail.com>
2026-05-11 21:47:53 +0700

    fix: memperbaiki error, menambahkan void ResumeGame dan penyesuaian lain

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum GameState { Playing, Paused, GameOver }
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     public GameState currentState;
-
     public GameObject pausePanel;
     public GameObject gameOverPanel;
 
@@ -37,12 +35,36 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         Time.timeScale = 0f;
+        if(pausePanel != null) pausePanel.SetActive(true);
         currentState = GameState.Paused;
+        Debug.Log("Game Paused");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        if(pausePanel != null) pausePanel.SetActive(false);
+        currentState = GameState.Playing;
+        Debug.Log("Game Resumed");
     }
 
     public void GameOver()
     {
         Debug.Log("Game Over");
+        Time.timeScale = 0f;
+        if(gameOverPanel != null) gameOverPanel.SetActive(true);
         currentState = GameState.GameOver;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Game Restarted");
+    }
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("gas pencet play");
+        SceneManager.LoadScene("MainMenu");
     }
 }
